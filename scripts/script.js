862ac7a796eed7a3e72f282e16df9dd6c925787c
Chibuzor Okoroafor <chibuzorokoroafor6@gmail.com>
2022-05-24 10:09:09 +0100

    Add operator function

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,15 @@ function multiplyNumbers (num1, num2) {
 function divideNumbers (num1, num2) {
     return num1 / num2
 }
+
+function operate(num1, num2, operator) {
+    if(operator === "add") {
+       return addNumbers(num1, num2)
+    } else if(operator === "subtract") {
+       return subtractNumbers(num1, num2)
+    } else if(operator === "multiply") {
+       return multiplyNumbers(num1, num2)
+    } else if(operator === "divide") {
+       return divideNumbers(num1, num2)
+    }
+}
